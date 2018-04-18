1d9e607c43b6b45d9ee669758ffacaffaee9765e
acacheung <acacheung@gmail.com>
2018-04-19 00:48:38 +0100

    day n night theming

--- scripts.js
+++ scripts.js
@@ -5,6 +5,7 @@ window.onload = function() {
     document.getElementById('js-logan').style.backgroundImage = 'url(images/logan--day.png)';
     document.getElementById('js-matt').style.backgroundImage = 'url(images/matt--day.png)';
     document.getElementById('js-kevin').style.backgroundImage = 'url(images/kevin--day.png)';
+    document.getElementById('js-download').src = 'images/download--day.svg';
   }
   document.getElementById('js-night').onclick = function() {
     document.documentElement.style.setProperty('--primary', '#fdc82e');
@@ -12,5 +13,6 @@ window.onload = function() {
     document.getElementById('js-logan').style.backgroundImage = 'url(images/logan--night.png)';
     document.getElementById('js-matt').style.backgroundImage = 'url(images/matt--night.png)';
     document.getElementById('js-kevin').style.backgroundImage = 'url(images/kevin--night.png)';
+    document.getElementById('js-download').src = 'images/download--night.svg';
   }
 }
