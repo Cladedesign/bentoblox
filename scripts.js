7d52737f948e5bfeb5e4a2823e525b57533efb96
acacheung <acacheung@gmail.com>
2018-04-18 23:30:49 +0100

    Better day to night

--- scripts.js
+++ scripts.js
@@ -1,11 +1,16 @@
 window.onload = function() {
-  var toggleTrigger = document.getElementsByClassName("toggle");
-  var toggleBackground = function() {
-    var theme = this.getAttribute("id");
-    document.body.classList.remove("night");
-    document.body.classList.add(theme);
-  };
-  for (var i = 0; i < toggleTrigger.length; i++) {
-    toggleTrigger[i].addEventListener('click', toggleBackground, false);
+  document.getElementById('js-day').onclick = function() {
+    document.documentElement.style.setProperty('--primary', '#1eb7da');
+    document.documentElement.style.setProperty('--secondary', '#fff7dd');
+    document.getElementById('js-logan').style.backgroundImage = 'url(images/logan--day.png)';
+    document.getElementById('js-matt').style.backgroundImage = 'url(images/matt--day.png)';
+    document.getElementById('js-kevin').style.backgroundImage = 'url(images/kevin--day.png)';
+  }
+  document.getElementById('js-night').onclick = function() {
+    document.documentElement.style.setProperty('--primary', '#fdc82e');
+    document.documentElement.style.setProperty('--secondary', '#2a0a31');
+    document.getElementById('js-logan').style.backgroundImage = 'url(images/logan--night.png)';
+    document.getElementById('js-matt').style.backgroundImage = 'url(images/matt--night.png)';
+    document.getElementById('js-kevin').style.backgroundImage = 'url(images/kevin--night.png)';
   }
 }
